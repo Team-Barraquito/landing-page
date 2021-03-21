632d58aa271b39dd1e476839b26b0bd122844c25
JoseCova <alu0101023663@ull.edu.es>
2021-03-21 15:34:36 +0000

    Modified font-face rule

--- src/components/SlidingText.js
+++ src/components/SlidingText.js
@@ -1,8 +1,37 @@
 import { LitElement, html, css } from "lit-element";
 
 class SlidingText extends LitElement {
+  static get styles () {
+    return css`    
+      :host {
+        font-family: 'Monument Extended';
+        color: yellow;
+        font-size: 2.8rem;
+      }
+      
+      .text-container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin: auto;
+        width: 65%;
+        height: 100px;
+        background: black;
+      }
+
+      span {
+        padding: 10px;
+      }
+      `;
+  }
+
   render () {
-    return html`<div> Hola! </div>`;
+    return html`
+      <div class="text-container">
+        <span>Suscríbete /</span>
+        <span>Suscríbete /</span>
+        <span>Suscríbete /</span>
+      </div>`;
   }
 }
 
