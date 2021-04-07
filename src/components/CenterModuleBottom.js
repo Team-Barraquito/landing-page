53b815eea3f157ace59b6e74984763f548052fc2
JoseCova <alu0101023663@ull.edu.es>
2021-04-07 20:12:14 +0100

    indented correctly

--- src/components/CenterModuleBottom.js
+++ src/components/CenterModuleBottom.js
@@ -3,64 +3,64 @@ import { LitElement, html, css } from "lit-element";
 class CenterModuleBottom extends LitElement {
   static get styles () {
     return css`
-    :host {
-      width: 100%;
-      height: 100%;
-      font-family: "Monument Extended";
-      color: white;
-    }
+      :host {
+        width: 100%;
+        height: 100%;
+        font-family: "Monument Extended";
+        color: white;
+      }
 
-    :host .global-container {
-      width: 100%;
-      height: 50%;
-    }
+      :host .global-container {
+        width: 100%;
+        height: 50%;
+      }
 
-    :host .global-container .second-content {
-      width: 100%;
-      height: 100%;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
+      :host .global-container .second-content {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
 
-    :host .global-container .second-content .image-container {
-      height: 100%;
-      width: 50%;
-      overflow: hidden;
-      position: relative;
-    }
+      :host .global-container .second-content .image-container {
+        height: 100%;
+        width: 50%;
+        overflow: hidden;
+        position: relative;
+      }
 
-    :host .global-container .second-content .second-text-container {
-      height: 100%;
-      width: 50%;
-      display: flex;
-      justify-content: flex-end;
-      flex-direction: column;
-    }
+      :host .global-container .second-content .second-text-container {
+        height: 100%;
+        width: 50%;
+        display: flex;
+        justify-content: flex-end;
+        flex-direction: column;
+      }
 
-    :host .global-container .second-content .second-text-container h3 {
-      margin: 0;
-      font-size: 5.625rem;
-      text-align: left;
-      line-height: 5rem;
-      transform: translateX(-115px);
-    }
-
-    :host .global-container .second-content .second-text-container p {
-      margin: 0;
-      font-size: 1.25rem;
-      text-align: left;
-      padding: 3.65rem 0 0 0.625rem;
-    }
-
-    @media screen and (max-width: 768px) {
       :host .global-container .second-content .second-text-container h3 {
         margin: 0;
-        font-size: 4.25rem;
+        font-size: 5.625rem;
         text-align: left;
         line-height: 5rem;
         transform: translateX(-115px);
       }
+
+      :host .global-container .second-content .second-text-container p {
+        margin: 0;
+        font-size: 1.25rem;
+        text-align: left;
+        padding: 3.65rem 0 0 0.625rem;
+      }
+
+      @media screen and (max-width: 768px) {
+        :host .global-container .second-content .second-text-container h3 {
+          margin: 0;
+          font-size: 4.25rem;
+          text-align: left;
+          line-height: 5rem;
+          transform: translateX(-115px);
+        }
 
       :host .global-container .second-content .second-text-container p {
         margin: 0;
@@ -70,23 +70,22 @@ class CenterModuleBottom extends LitElement {
       }
     }
 
-    @media screen and (max-width: 375px) {
-      :host .global-container .second-content .second-text-container h3 {
-        margin: 0;
-        font-size: 2.25rem;
-        text-align: left;
-        line-height: 2.5rem;
-        transform: translateX(-30px);
-      }
+      @media screen and (max-width: 375px) {
+        :host .global-container .second-content .second-text-container h3 {
+          margin: 0;
+          font-size: 2.25rem;
+          text-align: left;
+          line-height: 2.5rem;
+          transform: translateX(-30px);
+        }
 
-      :host .global-container .second-content .second-text-container p {
-        margin: 0;
-        font-size: 0.8rem;
-        text-align: left;
-        padding: 3.65rem 0.625rem 0 0.625rem;
-      }
-    }
-`;
+        :host .global-container .second-content .second-text-container p {
+          margin: 0;
+          font-size: 0.8rem;
+          text-align: left;
+          padding: 3.65rem 0.625rem 0 0.625rem;
+        }
+      }`;
   }
 
   // TODO cambiar clases de aqui
@@ -95,7 +94,7 @@ class CenterModuleBottom extends LitElement {
       <div class="global-container">
         <div class ="second-content">
           <div class="image-container">
-            <img src ="../assets/images/Kincaid.jpg" alt="Exposición Kincaid"/>
+            <img src ="/images/Kincaid.jpg" alt="Exposición Kincaid"/>
           </div>
           <div class ="second-text-container">
             <h3> Basil Kincaid </h3>
