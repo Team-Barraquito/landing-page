8ee8854e4959ccd8152e08f3e35e8669a832e9a9
JoseCova <alu0101023663@ull.edu.es>
2021-04-07 20:12:36 +0100

    renamed file

---
+++ src/components/ThisMonth.js
@@ -0,0 +1,51 @@
+import { LitElement, html, css } from "lit-element";
+
+class ThisMonth extends LitElement {
+  static get styles () {
+    return css`
+    .this-month-container {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      align-items: flex-start;
+      justify-content: flex-start;
+    }
+
+    .this-month-png {
+      width: 1000px;
+      height: 100%;
+      background-image: url("/images/this-month-desktop.png");
+      background-repeat: no-repeat;
+      background-size: 100%;
+    }
+
+    @media screen and (max-width: 768px) {
+      .this-month-png {
+        width: 793px;
+        height: 438px;
+        background-image: url("/images/this-month-mobile.png");
+        background-repeat: no-repeat;
+        background-size: 100%;
+      }
+
+      .this-month-container {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: flex-start;
+        justify-content: flex-start;
+        overflow: hidden;
+      }
+    }
+`;
+  }
+
+  render () {
+    return html`
+    <div class="this-month-container">
+      <div class="this-month-png"></div>
+    </div>
+      `;
+  }
+}
+customElements.define("this-month", ThisMonth);
